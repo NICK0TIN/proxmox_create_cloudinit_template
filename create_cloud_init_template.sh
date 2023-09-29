44917beddc178e4a03b42b02fad8089029116a6a
root <root@pve.fritz.box>
2023-09-29 16:01:32 +0200

    tested

--- create_cloud_init_template.sh
+++ create_cloud_init_template.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
 
-TEMP_ID=1002
-TEMP_NAME="rhel9-cloud-init"
+TEMP_ID=1003
+TEMP_NAME="rocky9-cloud-init"
 #TEMP_NAME="rhel8-cloud-init"
 KVM_IMAGE_DIR="/opt/kvm_images"
 #CLOUD_ISO='rhel-8.2-x86_64-kvm.qcow2'
-CLOUD_ISO='rhel-9.2-x86_64-kvm.qcow2'
-
+#CLOUD_ISO='rhel-9.2-x86_64-kvm.qcow2'
+CLOUD_ISO='rocky-9.0-x86_64-kvm.qcow2'
 qm create $TEMP_ID --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-single -name $TEMP_NAME
 
 #qm create 1002 --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-single -name deleteme
