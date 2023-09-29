fbeef8e4ae54cef2f91a9a50dcf72513fea497f0
root <root@pve.fritz.box>
2023-09-29 11:37:12 +0200

    tested

--- create_cloud_init_template.sh
+++ create_cloud_init_template.sh
@@ -1,14 +1,20 @@
 #!/bin/bash 
 
-TEMP_ID=1001
-TEMP_NAME="RHEL9 CLOUD INIT"
-TEMP_NAME="RHEL8 CLOUD INIT"
-CLOUD_ISO='/opt/rhel-8.2-x86_64-kvm.qcow2'
+TEMP_ID=1002
+TEMP_NAME="rhel9-cloud-init"
+#TEMP_NAME="rhel8-cloud-init"
+KVM_IMAGE_DIR="/opt/kvm_images"
+#CLOUD_ISO='rhel-8.2-x86_64-kvm.qcow2'
+CLOUD_ISO='rhel-9.2-x86_64-kvm.qcow2'
 
+qm create $TEMP_ID --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-single -name $TEMP_NAME
 
-qm create $TEMP_ID --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-single
-qm set $TEMP_ID --scsi0 local-lvm:0,import-from=$CLOUD_ISO
+#qm create 1002 --memory 2048 --net0 virtio,bridge=vmbr0 --scsihw virtio-scsi-single -name deleteme
+qm set $TEMP_ID --scsi0 local-lvm:0,import-from=$KVM_IMAGE_DIR/$CLOUD_ISO
+sleep 0.5
 qm set $TEMP_ID --ide2 local-lvm:cloudinit
+sleep 0.5
 qm set $TEMP_ID --boot order=scsi0
+sleep 0.5
 qm template $TEMP_ID
    
